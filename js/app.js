c105c4c09e7233ebc8ddc52bbdb9d0c99c73f4e4
Thierry Legros <thierry.legros2@club.fr>
2019-06-25 09:52:08 +0200

    test tab2

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-const name = 'Thierry';
-const email = 'thierry@mail.fr';
-const age = 14;
-const man = true;
+// const name = 'Thierry';
+// const email = 'thierry@mail.fr';
+// const age = 14;
+// const man = true;
 
-const tab = [3, 4,true, 'salut', 8, 12];
-tab[tab.length-1];
-console.log(name, tab);
-console.log('index: ',tab.length-1);
-console.log('valeur: ',tab[5]);
+// const tab = [3, 4,true, 'salut', 8, 12];
+// tab[tab.length-1];
+// console.log(name, tab);
+// console.log('index: ',tab.length-1);
+// console.log('valeur: ',tab[5]);
+
+const tab = ['Georges', 65];
+const obj = {firstname: 'Georges', age: 65};
+
+console.log(tab[0] + ' a '+ tab[1] +' ans');
+console.log(obj['firstname'] + ' a ' + obj['age'] + ' ans');
+console.log(obj.firstname + ' a ' + obj.age + ' ans');
