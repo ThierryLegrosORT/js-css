ce21833d84b35a8a54d51f82ad34408f8762007f
Thierry Legros <thierry.legros2@club.fr>
2019-06-25 09:39:10 +0200

    test tableaux

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const name = 'Thierry';
+const email = 'thierry@mail.fr';
+const age = 14;
+const man = true;
+
+const tab = [3, 4,true, 'salut', 8, 12];
+tab[tab.length-1];
+console.log(name, tab);
+console.log('index: ',tab.length-1);
+console.log('valeur: ',tab[5]);
