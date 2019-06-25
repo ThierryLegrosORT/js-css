a1011747ce97fbbf1d8840daf394480784bdd249
Thierry Legros <thierry.legros2@club.fr>
2019-06-25 09:59:41 +0200

    les objets - intro

--- js/app.js
+++ js/app.js
@@ -9,9 +9,13 @@
 // console.log('index: ',tab.length-1);
 // console.log('valeur: ',tab[5]);
 
+// OBJET
 const tab = ['Georges', 65];
 const obj = {firstname: 'Georges', age: 65};
 
+const maPropriete = 'firstname';
+
 console.log(tab[0] + ' a '+ tab[1] +' ans');
 console.log(obj['firstname'] + ' a ' + obj['age'] + ' ans');
+console.log(obj[maPropriete] + ' a ' + obj['age'] + ' ans');
 console.log(obj.firstname + ' a ' + obj.age + ' ans');
